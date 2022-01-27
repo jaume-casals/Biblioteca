5d010425a77dd6001bf1b50cd5058eb0b45281fc
Jordicasalsvilaplana <jordicasals29@gmail.com>
2022-01-27 18:23:06 +0100

    ficat filtre updated gitignore

--- src/presentacio/MostrarBibliotecaPanel.java
+++ src/presentacio/MostrarBibliotecaPanel.java
@@ -1,30 +1,61 @@
 package presentacio;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.border.LineBorder;
 
 import herramienta.ConfiguracionPantalla;
 
 import javax.swing.JScrollPane;
 
 public class MostrarBibliotecaPanel extends JPanel {
-	private JTable table;
-	private JScrollPane scrollPane;
 
 	private ConfiguracionPantalla configuracionPantalla = new ConfiguracionPantalla();
 
+	private JTable table;
+	private JScrollPane scrollPane;
+	private JScrollPane scrolpaneFiltro;
+	private JPanel panelFiltros;
+
+	private JButton bttnFiltrar;
+	private JButton bttnQuitarFiltros;
+
 	public MostrarBibliotecaPanel() {
 		setLayout(null);
+
+		// Table
 		scrollPane = new JScrollPane();
 
-		scrollPane.setBounds(amplada(50)-5, altura(0)+10, amplada(120), altura(52));
+		scrollPane.setBounds(amplada(50) - 5, altura(0) + 10, amplada(120), altura(52));
 		add(scrollPane);
 
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		// prova
+		// Filtre
+
+		scrolpaneFiltro = new JScrollPane();
+		scrolpaneFiltro.setBounds(10, 11, amplada(40), altura(52));
+		add(scrolpaneFiltro);
+
+		panelFiltros = new JPanel();
+		panelFiltros.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		scrolpaneFiltro.setViewportView(panelFiltros);
+		panelFiltros.setLayout(null);
+
+		bttnFiltrar = new JButton("Filtrar");
+		bttnFiltrar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		bttnFiltrar.setBounds(amplada(10), altura(44), amplada(20), altura(2));
+		panelFiltros.add(bttnFiltrar);
+
+		bttnQuitarFiltros = new JButton("Quitar filtros");
+		bttnQuitarFiltros.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		bttnQuitarFiltros.setBounds(amplada(10), altura(48), amplada(20), altura(2));
+		panelFiltros.add(bttnQuitarFiltros);
 
 	}
 
