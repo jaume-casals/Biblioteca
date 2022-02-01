aae41e96d5200541c8b9501e39d5d42a3f99fc06
Jordicasalsvilaplana <jordicasals29@gmail.com>
2022-02-01 20:49:18 +0100

    changed main control

--- src/presentacio/MainFrameControl.java
+++ src/presentacio/MainFrameControl.java
@@ -1,21 +1,23 @@
 package presentacio;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import domini.Llibre;
 import presentacio.acercade.AcercaDeDialogoControl;
+import presentacio.detalles.vista.GuardarLlibresDialogo;
+import presentacio.detalles.vista.GuardarLlibresDialogoControl;
 import presentacio.acercade.AcercaDeDialogo;
 import persistencia.ControladorPersistencia;
 import persistencia.ServerConect;
 
 public class MainFrameControl {
 
-	private static ServerConect con = new ServerConect();
-	private static ControladorPersistencia cLlibres;
-	private static ArrayList<Llibre> biblio;
-	private static MostrarBibliotecaControl MostrarBibliotecaControl;
-	private static MainFramePanel vista;
+	private ServerConect con = new ServerConect();
+	private ControladorPersistencia cLlibres;
+	private ArrayList<Llibre> biblio;
+	private MostrarBibliotecaControl MostrarBibliotecaControl;
+	private MainFramePanel vista;
+	private GuardarLlibresDialogoControl guardarLlibresDialogoControl;
 
 	public MainFrameControl(MainFramePanel vista) {
 		this.vista = vista;
@@ -27,11 +29,7 @@ public class MainFrameControl {
 //		biblio = con.getAllLlibres();
 
 //		cLlibres = new ControladorLlibres(con.getConnection(), biblio);
-		this.vista.getMntmAbout().addActionListener(
-				e -> new Thread(() -> new AcercaDeDialogoControl(new AcercaDeDialogo()).setVisible(true)).start());
 
-		MostrarBibliotecaControl = new MostrarBibliotecaControl(this.vista.getMostrarBibliotecaPanel(), biblio,
-				con.getHeader());
 //		cLlibres.afegirLlibre(new Llibre(1, "ala", "ala", 1231, "dsakfljhasdl�kfjhasldf", 100.01, 10.3, Boolean.TRUE));
 
 //		for (int i = 0; i < biblio.size(); i++) {
@@ -47,6 +45,18 @@ public class MainFrameControl {
 
 //		con.closeConection();
 
+		this.vista.getaddLlibre().addActionListener(e -> new Thread(() -> crearLlibreDialogo()).start());
+		this.vista.getMntmAbout().addActionListener(
+				e -> new Thread(() -> new AcercaDeDialogoControl(new AcercaDeDialogo()).setVisible(true)).start());
+
+		MostrarBibliotecaControl = new MostrarBibliotecaControl(this.vista.getMostrarBibliotecaPanel(), biblio,
+				con.getHeader());
+	}
+
+	private void crearLlibreDialogo() {
+		this.guardarLlibresDialogoControl = new GuardarLlibresDialogoControl(new GuardarLlibresDialogo());
+		this.guardarLlibresDialogoControl.getVista().setLocationRelativeTo(this.vista);
+		this.guardarLlibresDialogoControl.getVista().setVisible(true);
 	}
 
 	public void setVisible(boolean b) {
